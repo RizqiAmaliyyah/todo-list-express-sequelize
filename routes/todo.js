fced9d9edef6fd2a675e315bc5a38246cc4ee378
RizqiAmaliyyah <rizqia439@gmail.com>
2024-01-15 19:53:30 +0700

    buat route todo.js tapi sepertinya belum bisa jalan juga, karena masih belum paham caranya

--- routes/todo.js
+++ routes/todo.js
@@ -1,12 +1,21 @@
 const express = require("express")
+const { getTodo, getTodoDetail, createTodo, updateTodo, deleteTodo } = require("../controllers/todo");
 
 const userController = require('../controllers/user')
 const authController = require('../middleware/auth')
+
+const todoController = require('../controllers/todo')
 // const auth = require("./auth")
 
 // express.Router() fungsinya ngebantu kita supaya routernya dibuat sesuai kategori
-const user = express.Router()
+const todo = express.Router()
 
-user.get('/see-profile', authMiddleware.verifyToken, userController.getUsername)
+todo.get('/todos', getTodo);
+todo.get('/todos/:id', getDetailTodo);
+todo.post('/todos', createTodo);
+todo.patch('/todos/:id', updateTodo);
+todo.delete('/todos/:id', deleteTodo);
 
-module.exports = user
+module.exports = {
+  todo
+}
