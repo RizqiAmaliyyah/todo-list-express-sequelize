91532a789636dd450251e323fe74dfc0933a0e3f
RizqiAmaliyyah <rizqia439@gmail.com>
2024-01-15 18:31:40 +0700

    melakukan db:seed:all

--- seeders/20240115105956-membuat-todo.js
+++ seeders/20240115105956-membuat-todo.js
@@ -1,25 +1,59 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("todos", [
+      {
+        username: "Tapioka",
+        tittle: "todo 1",
+        description: "Beli tepung",
+        startTime: new Date(),
+        status: "false",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        username: "Minyak",
+        tittle: "todo 2",
+        description: "Beli minyak makan",
+        startTime: new Date(),
+        status: "false",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        username: "Minum",
+        tittle: "todo 3",
+        description: "Beli galon besar dua",
+        startTime: new Date(),
+        status: "false",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        username: "Lauk",
+        tittle: "todo 4",
+        description: "Beli untuk makan malam",
+        startTime: new Date(),
+        status: "false",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        username: "Tidur",
+        tittle: "todo 5",
+        description: "Tidur nomor satu, yang lain nanti dulu",
+        startTime: new Date(),
+        status: "true",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("todos", null, {});
+  },
 };
