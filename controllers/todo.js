acd44278f7cd6277894271c5507b16b6e8fdabbd
RizqiAmaliyyah <rizqia439@gmail.com>
2024-01-15 18:56:13 +0700

    menambahkan get todo by Id

--- controllers/todo.js
+++ controllers/todo.js
@@ -13,3 +13,20 @@ const TodoModel = require('./todos', async function (req, res) {
     });
   }
 });
+
+// get todo detail by Id
+const TodoModel = require('./models').Todo;
+
+app.get('/todos/:todoId', async function (req, res) {
+  try {
+    const { todoId } = req.params;
+    const todo = await TodoModel.findOne({id: Number(todoId)});
+
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || 'internal server error try again later',
+    });
+  }
+});
+
